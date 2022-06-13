56b99bebf835a5c61493f46ca2e393b876c05f3a
Lameck Irungu <lameckirungu90@gmail.com>
2022-06-13 07:26:32 -0700

    adds a program to updatethe value of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * void reset_to_98 - takes a pointer to an int as param
+ * and updates the value it points to
+ *
+ * Return updated value
+ */
+
+void reset_to_98(int *n)
+{
+      *n = 98;
+}
