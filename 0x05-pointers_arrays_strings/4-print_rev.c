dcd73155ffa91e7e09f823e109e1389651914b65
Lameck Irungu <lameckirungu90@gmail.com>
2022-06-13 10:12:06 -0700

    printing numbers in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverse mode
+ * @str: The string to be printed
+ *
+ * Return: void
+ */
+void print_rev(str)
+{
+	int i;
+
+	i = strlen(str) - 1;
+
+	for (; *str != *s[0]; s--)
+	{
+		_putchar(str);
+	}
+}
+
