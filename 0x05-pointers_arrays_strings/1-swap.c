171a77d865c8c27fd738197acb19d0f7eacff4ff
Lameck Irungu <lameckirungu90@gmail.com>
2022-06-13 08:14:21 -0700

    adds a program that swaps two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: An integer to swap
+ * @b: Another int to swap
+ * 
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int num;
+	
+	/*value of a is assigned to num*/
+       	num = *a;
+	/*value of b is assigned to a*/
+	*a = *b;
+	/*value of b is assigned to num*/
+	*b = num;
+}
