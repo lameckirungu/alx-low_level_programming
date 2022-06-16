12ce20b68571efdd35ae221dc3f11ecc5e985aee
Lameck Irungu <lameckirungu90@gmail.com>
2022-06-16 02:17:47 -0700

    more pointers arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -8,3 +11,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
+
+#endif
